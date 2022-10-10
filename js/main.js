6354a397f151c8ff0713f8692bb55d79aa608d2f
Mane <a.manasijevic@outlook.com>
2022-10-10 15:14:57 +0200

    mobile menu done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 let mybutton = document.getElementById("btn-scroll-top");
+let navigation = document.getElementById("js-navbar");
+let menuClose = document.getElementById("js-close-menu");
+let body = document.querySelector("body");
+
+// Video autoplay
+let videoPromo = document.getElementById("video-promo");
+videoPromo.autoplay = true;
+videoPromo.load();
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -11,21 +19,57 @@ function scrollFunction() {
   }
 }
 
+function openMenu() {
+  navigation.classList.add("is-active");
+  body.classList.add("lock");
+  menuClose.classList.add('is-active');
+}
+
+function closeMenu() {
+  navigation.classList.remove("is-active");
+  body.classList.remove("lock");
+  menuClose.classList.remove('is-active');
+}
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-document.addEventListener( 'DOMContentLoaded', function () {
-//slider
-let splide = new Splide(".js-splide-partners", {
-  type: "loop",
-  focus: "center",
-  arrows: true,
-  pagination: true,
-  perPage: 5,
+
+// Slider for partners
+document.addEventListener('DOMContentLoaded', function () {
+  let swiper = new Swiper(".js-brand-slider", {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    loop: true,
+    pagination: {
+      el: ".js-brand-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.js-swiper-next-brand',
+      prevEl: '.js-swiper-prev-brand'
+    },
+    breakpoints: {
+      1400: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+    },
+  });
+
 });
 
-splide.mount();
-
-} );
